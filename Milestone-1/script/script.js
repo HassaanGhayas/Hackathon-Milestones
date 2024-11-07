fee458b50770d1628d8cf1f6122fc8588aec3aae
Hassaan <h.n.ghayas@gmail.com>
2024-11-07 21:48:53 +0500

    Fixed compile problem

--- Milestone-1/script/script.js
+++ Milestone-1/script/script.js
@@ -1,16 +1,19 @@
-var ViewSkills = document.getElementById("toggle-btn");
-var condition = false;
-ViewSkills.addEventListener("click", function () {
-  var visibility = document.querySelector(".main-section");
-  if (condition) {
-    visibility.style.display = "grid";
-    ViewSkills.innerText = "Hide Details";
-    condition = false;
-    ViewSkills.style.marginTop = "0";
-  } else {
-    visibility.style.display = "none";
-    condition = true;
-    ViewSkills.innerText = "View Details";
-    ViewSkills.style.marginTop = "20px";
-  }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let ViewSkills = document.getElementById("toggle-btn");
+let condition = false;
+ViewSkills.addEventListener("click", () => {
+    let visibility = document.querySelector(".main-section");
+    if (condition) {
+        visibility.style.display = "grid";
+        ViewSkills.innerText = "Hide Details";
+        condition = false;
+        ViewSkills.style.marginTop = "0";
+    }
+    else {
+        visibility.style.display = "none";
+        condition = true;
+        ViewSkills.innerText = "View Details";
+        ViewSkills.style.marginTop = "20px";
+    }
 });
